ae757f2054028be1fd09fec6bdacab9905b594f0
bambiru <dj70083798@gmail.com>
2024-01-21 22:29:36 +0900

    [🎨Modify]: header 추가
    - header 컴포넌트 추가

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { createRoot } from "react-dom/client";
+import { Header } from "./components/Header";
 import { Detail } from "./Detail";
 
 const root = document.querySelector("#root");
 
 const render = (
   <>
+    <Header />
     <Detail />
   </>
 );
